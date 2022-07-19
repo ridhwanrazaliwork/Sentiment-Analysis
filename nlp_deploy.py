615d8f86a24e421b8610520833e64dac3c4e8aa8
Ridhwan Razali <ridhwanrazaliwork@gmail.com>
2022-07-19 09:32:17 +0800

    Initial commit
    Added main files

---
+++ nlp_deploy.py
@@ -0,0 +1,72 @@
+#%%
+# Tokenizer
+import pickle
+import re
+import json
+import os
+import streamlit as st
+import numpy as np
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+#%%
+# Model Loading
+# Tokenizer
+TOKENIZER_PATH = os.path.join(os.getcwd(),'model', 'tokenizer.json')
+MODEL_PATH = os.path.join(os.getcwd(),'model','model.h5')
+OHE_PATH = os.path.join(os.getcwd(),'model', 'ohe.pkl')
+
+with open(TOKENIZER_PATH,'r') as file:
+    loaded_tokenizer = json.load(file)
+
+tokenizer = tokenizer_from_json(loaded_tokenizer)
+
+# Model
+
+model = load_model(MODEL_PATH)
+
+model.summary()
+
+# Ohe
+with open(OHE_PATH,'rb') as file:
+    ohe = pickle.load(file)
+# Model
+# %%
+
+# new_review = ["Watched It in IMAX, wonderful, epic! no SPOILERS for anyone here. The Movie is truly not as good as Deadpool Part 1, as Part 1 had a trustworthy storyline,  A hero, a Villian, hero has to take revenge from the Villian who did him wrong, But Deadpool 2 is world's Apart, There is no specific Villian Segregated in the Movie, watch it and you'll come to know, The action scenes are totally awesome, while some of the scenes including Gore and blood are cut, The movie is one of the Most ENTERTAINING movie ever produced. It isn't the best superhero movie, it can't even get close to Infinity War. But it is the most enjoyable movie i've ever seen. Lots Of Comedy, Lots of Action, lots of fourth wall breaking humor, Lots of twists and cameos, It's unpredictable and truly enjoyable, but The only drawback of watching this movie is that your lungs will start hurting, beware not to Die Laughing! and also that all entertaining things end within the blink of an eye. Yes it seems like a 10 minute film to me, compared to Infinity war, as It is enjoyed and lacks even a bit of seriousness, whereas Infinity war still has an impression of a Lifelong Infinite Movie That Never Ends. At the End If You're Wondering whether you should watch it or not just Go For It, It is the most entertaining movie in The Recent Times, The Cameos and twists in The Story will blow your mind!"]
+new_review = [input('Please type your review here')]
+st.text_input('Movie title', 'Life of Brian')
+for index, text in enumerate(new_review):
+    # to remove html tags
+    new_review = re.sub('<.*?>','',text)
+    new_review = re.sub('[^a-zA-Z]',' ',new_review).lower().split()
+
+# DAta preprocessing
+new_review = tokenizer.texts_to_sequences(new_review)
+new_review = np.reshape(new_review,(1,len(new_review)))
+new_review = pad_sequences(new_review,maxlen=178,padding='post', truncating='post')
+# %% model prediction
+
+outcome = model.predict(new_review)
+
+print(f'This review is {ohe.inverse_transform(outcome)[0][0]}')
+# %%
+# Streamlit
+with st.form("Heart Attack Predictor App"):
+    chol = st.selectbox('Cholesterol Level: 1: normal, 2: above normal, 3: well above normal', (1,2,3))
+    sys = st.number_input('Systolic BP')
+    dia = st.number_input('Diastolic BP')
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        new_data = np.expand_dims([chol,sys,dia],axis=0)
+        outcome = model.predict(new_data)[0]
+        
+        if outcome == 0:
+            st.write('Congrats you are healthy, keep it up')
+            st.balloons()
+        else:
+            st.write('Train Harder!!!')
+            st.snow()
---
+++ nlp_training.py
@@ -0,0 +1,146 @@
+#%%
+import os
+import pandas as pd
+import re
+# %%
+# Data loading
+df = pd.read_csv('https://raw.githubusercontent.com/Ankit152/IMDB-sentiment-analysis/master/IMDB-Dataset.csv')
+
+# Data Inspection
+df.info()
+df.describe().T
+df.head()
+
+df.duplicated().sum()
+df.isna().sum()
+print(df['review'][4])
+print(df['review'][10])
+
+# Need to clean HTML tags and symbol
+# %% Data cleaning
+# test = df['review'][4]
+# print('Before')
+# print(test)
+# print('After')
+# test = re.sub('<.*?>','',test)
+# test = re.sub('[^a-zA-Z]',' ',test).lower().split()
+# print(test)
+
+review = df['review']
+sentiment = df['sentiment']
+
+for index, text in enumerate(review):
+    # to remove html tags
+    review[index] = re.sub('<.*?>','',text)
+    review[index] = re.sub('[^a-zA-Z]',' ',text).lower().split()
+
+review_backup = review.copy()
+sentiment_backup = sentiment.copy()
+# %%
+# Data preprocessing
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+vocab_size = 10000
+oov_token = '<OOV>'
+
+tokenizer = Tokenizer(num_words=vocab_size,oov_token=oov_token)
+
+tokenizer.fit_on_texts(review)
+word_index = tokenizer.word_index
+
+print(dict(list(word_index.items())[0:10]))
+
+review_int = tokenizer.texts_to_sequences(review)
+
+# length_review = []
+# for i in range(len(review_int)):
+#     length_review.append(len(review_int[i]))
+#     # print(len(review_int[i]))
+
+# np.median(length_review)
+
+# shortcut
+max_len = np.median([len(review_int[i]) for i in range(len(review_int))])
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+padded_review = pad_sequences(review_int,
+                            maxlen=int(max_len),
+                            padding='post',
+                            truncating='post')
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+ohe = OneHotEncoder(sparse=False)
+sentiment = ohe.fit_transform(np.expand_dims(sentiment,axis=-1))
+
+X_train,X_test,y_train,y_test = train_test_split(padded_review,sentiment,
+                                                test_size=0.3,
+                                                random_state=123)
+# %% model development
+
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras import Input,Sequential
+from tensorflow.keras.layers import  Embedding, Bidirectional
+
+# X_train = np.expand_dims(X_train,axis=-1)
+# X_test = np.expand_dims(X_test,axis=-1)
+input_shape = np.shape(X_train)[1:]
+out_dim = 128
+# LSTM/RNN?GRU must be rank 3 tensor
+model = Sequential()
+model.add(Input(shape=(input_shape)))
+model.add(Embedding(vocab_size,out_dim)) #need two dimension
+model.add(Bidirectional(LSTM(128,return_sequences=True)))#LSTM will adjust be 3d
+model.add(Dropout(0.3))
+model.add(Bidirectional(LSTM(128)))
+model.add(Dropout(0.3))
+model.add(Dense(2,activation='softmax'))
+model.summary()
+
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['acc'])
+
+#%%
+from tensorflow.keras.callbacks import TensorBoard
+import datetime
+
+LOGS_PATH = os.path.join(os.getcwd(),'Logs',datetime.datetime.now().
+                    strftime('%Y%m%d-%H%M%S'))
+                    
+tensorboard_callback = TensorBoard(log_dir=LOGS_PATH,histogram_freq=1)
+
+
+
+hist = model.fit(X_train,y_train,epochs=5,
+                    validation_data=(X_test,y_test),   
+                    callbacks=[tensorboard_callback])
+
+# %%
+
+from sklearn.metrics import classification_report
+
+y_pred = np.argmax(model.predict(X_test),axis=1)
+y_actual = np.argmax(y_test,axis=1)
+
+print(classification_report(y_actual,y_pred))
+
+# %%
+
+# Tokenizer
+import json
+TOKENIZER_SAVE_PATH = os.path.join(os.getcwd(),'model','tokenizer.json')
+token_json = tokenizer.to_json()
+
+with open(TOKENIZER_SAVE_PATH, 'w') as file:
+    json.dump(token_json,file)
+# OHE
+import pickle
+OHE_SAVE_PATH = os.path.join(os.getcwd(),'model','ohe.pkl')
+
+with open(OHE_SAVE_PATH,'wb') as file:
+    pickle.dump(ohe,file)
+# Model
+MODEL_SAVE_PATH = os.path.join(os.getcwd(), 'model', 'model.h5')
+model.save(MODEL_SAVE_PATH)
